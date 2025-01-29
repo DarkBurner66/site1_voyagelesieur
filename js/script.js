cf9702886ef300930a878e9297812c901838d84a
DarkBurner66 <dominik_saint-denis@outlook.com>
2025-01-29 14:52:28 -0500

    javascript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const btnPlus = document.querySelector(".js-plus");
+const colTransport = document.querySelector(".conteneur-transport");
+
+btnPlus.addEventListener("click",ouvreferme);
+
+function ouvreferme(){
+    colTransport.classList.toggle("invisible");
+    
+}
